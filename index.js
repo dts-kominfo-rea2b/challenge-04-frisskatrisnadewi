c6234a32e166764cdcc96a69c1245a2226d01636
frisskatrisnadewi <frisskatrisnadewi@gmail.com>
2022-06-03 00:05:56 +0800

    challenge 3

--- index.js
+++ index.js
@@ -7,8 +7,40 @@ const dates = [
   "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
 ];
 
+
+
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (arrDates,position)=>{
+  const datasPosition= arrDates[position]
+  let allDatesArr=[]
+ // console.log(pos)
+  for(i=0;i<arrDates.length;i++){
+       
+    if(arrDates[i]==datasPosition){
+      let indexDateske= arrDates[i]
+      let parsingToScnd= Date.parse(indexDateske)/1000
+      let cnvrtStr=parsingToScnd.toString()
+      return cnvrtStr
+    // }else if (daten[i] !== pos){
+    //   let tg= daten[i]
+    //   let cv= Date.parse(tg)/1000
+    //   let cf= cv.toString()
+    //   let result = 
+    //   u.push(cf)
+      
+    }   
+      let keepAllDates= arrDates[i]
+      let parsingToScnd2= Date.parse(keepAllDates)/1000
+      let cnvrtStr2= parsingToScnd2.toString()
+      allDatesArr.push(cnvrtStr2)
+     
+  }
+  let sortir=allDatesArr.sort()
+  let hasil= sortir.join("-")
+  return hasil
+
+};
+
 
 // ! JANGAN DIMODIFIKASI
 (() => {
